1c9b3b9f82673e600cc288cf715d71924ff314fc
David Werth <info@davidwerth.com>
2023-10-21 16:04:15 +0200

    added update and create actions

--- libs/actions/menu.actions.js
+++ libs/actions/menu.actions.js
@@ -14,13 +14,50 @@ export async function getAllMenuItems() {
 	}
 }
 
-// export async function getMenuItemsInCart() {
-// 	try {
-// 		connectMongoDB();
+export async function createMenuItem({
+	category,
+	title,
+	description,
+	price,
+	img,
+}) {
+	try {
+		await connectMongoDB();
 
-// 		const menuItems = await JSON.parse(JSON.stringify(await MenuItem.find()));
-// 		return menuItems;
-// 	} catch (error) {
-// 		console.log(error);
-// 	}
-// }
+		await MenuItem.create({
+			category,
+			title,
+			description,
+			price,
+			img,
+		});
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+export async function updateMenuItem({
+	id,
+	category,
+	title,
+	description,
+	price,
+	img,
+}) {
+	try {
+		await connectMongoDB();
+
+		await MenuItem.findOneAndUpdate(
+			{ _id: id },
+			{
+				category: category,
+				title: title,
+				description: description,
+				price: price,
+				img: img,
+			}
+		);
+	} catch (error) {
+		console.log(error);
+	}
+}
