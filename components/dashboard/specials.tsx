aba33b89bf8d8d6f35f22f5457ce856fec066288
David Werth <info@davidwerth.com>
2023-10-24 06:45:19 +0200

    typo fix

--- components/dashboard/specials.tsx
+++ components/dashboard/specials.tsx
@@ -1,7 +1,7 @@
 export default function Specials() {
 	return (
 		<div className="flex flex-col h-full p-5 border rounded-lg border-neutral-500 text-neutral-200">
-			<h2 className="text-lg font-bold">Special</h2>
+			<h2 className="text-lg font-bold">Specials</h2>
 		</div>
 	);
 }
