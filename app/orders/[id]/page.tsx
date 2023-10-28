eb353f8d89a746741604f3d8c339c4aadaa60634
David Werth <info@davidwerth.com>
2023-10-28 09:47:12 +0200

    orders id page init

--- app/orders/[id]/page.tsx
+++ app/orders/[id]/page.tsx
@@ -1,3 +1,26 @@
 export default async function Page() {
-	return <div>id</div>;
+	return (
+		<main className="w-full pt-24 px-10 pb-10 max-w-7xl grid grid-cols-3">
+			<div className="flex flex-col h-full p-5 border rounded-lg border-neutral-500 text-neutral-200">
+				<div className="flex justify-between">
+					<h2 className="text-lg font-bold">Customer Details</h2>
+				</div>
+				<div className="flex flex-col  h-full">details</div>
+			</div>
+
+			<div className="flex flex-col h-full p-5 border rounded-lg border-neutral-500 text-neutral-200">
+				<div className="flex justify-between">
+					<h2 className="text-lg font-bold">Customer Details</h2>
+				</div>
+				<div className="flex flex-col  h-full">details</div>
+			</div>
+
+			<div className="flex flex-col h-full p-5 border rounded-lg border-neutral-500 text-neutral-200">
+				<div className="flex justify-between">
+					<h2 className="text-lg font-bold">Items</h2>
+				</div>
+				<div className="flex flex-col  h-full">details</div>
+			</div>
+		</main>
+	);
 }
